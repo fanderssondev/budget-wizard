d23254ff8857e144779fbdfab22c32bb06dbf471
nanobyter <sotarfredrik@gmail.com>
2023-09-26 21:49:56 +0200

    Pages created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import './app.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
