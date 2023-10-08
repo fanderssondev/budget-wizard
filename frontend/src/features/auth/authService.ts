49d1419c39264bdc9b6330eedf4c3226ce0aa822
nanobyter <sotarfredrik@gmail.com>
2023-10-08 18:41:40 +0200

    Working on typing react-redux correctly

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -5,19 +5,16 @@ const API_URL = '/api/users/';
 
 // Register user
 const register = async (userData: User) => {
-  const response = await axios.post(API_URL, userData);
-  console.log(response);
+  try {
+    const response = await axios.post(API_URL, userData);
+    if (response.data) {
+      localStorage.setItem('user', JSON.stringify(response.data));
+    }
 
-  // try {
-  //   const response = await axios.post(API_URL, userData);
-  //   if (response.data) {
-  //     localStorage.setItem('user', JSON.stringify(response.data));
-  //   }
-
-  //   return response.data;
-  // } catch (error) {
-  //   throw error;
-  // }
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const authService = {
